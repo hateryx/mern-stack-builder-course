70d6298902f752f91bab72e4f9033c698a488d33
Richmond S. Damag <rsdamag.cpa.8@gmail.com>
2022-12-08 10:49:10 +0800

    Update: connect.js
    Updated the connect.js to remove unnecessary string which exposes sensitive info

--- db/connect.js
+++ db/connect.js
@@ -1,8 +1,5 @@
 import mongoose from "mongoose";
 
-const connectionString =
-  "mongodb+srv://hateryx:1234@nodeexpressprojects.klyr3du.mongodb.net/?retryWrites=true&w=majority";
-
 const connectDB = (url) => {
   return mongoose.connect(url);
 };
