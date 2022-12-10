eb696c293c302026d43323196b5fd97fd4e6efcd
Richmond S. Damag <rsdamag.cpa.8@gmail.com>
2022-12-10 14:22:33 +0800

    Update: error-handler (middleware)
    - Updated codes for using http-status-code
    - Defined conditions (IF) to return error message dynamically based on the result of input validation

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,6 +1,30 @@
+import { StatusCodes } from "http-status-codes";
+
 const errorHandlerMiddleware = (err, req, res, next) => {
   console.log(err);
-  res.status(500).json({ msg: err });
+
+  const defaultError = {
+    statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: "Something went wrong, try again later.",
+  };
+
+  if (err.name === "ValidationError") {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    // defaultError.msg = err.message;
+    defaultError.msg = Object.values(err.errors)
+      .map((elem) => elem.message)
+      .join(",");
+  }
+
+  if (err.code && err.code === 11000) {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    defaultError.msg = `${Object.keys(
+      err.keyValue
+    )} provided belongs to an existing user. Please provide another email.`;
+  }
+  // (prototype) res.status(defaultError.statusCode).json({ msg: err });
+
+  res.status(defaultError.statusCode).json({ msg: defaultError.msg });
 };
 
 export default errorHandlerMiddleware;
