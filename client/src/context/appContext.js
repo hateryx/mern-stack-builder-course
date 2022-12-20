89ec54ab970964ccb1d1e0004d7d07531e2f7b7f
Richmond S. Damag <rsdamag.cpa.8@gmail.com>
2022-12-20 21:01:13 +0800

    Update: actions.js, appContext and reducer.js
    - Defined register user events (e.g. REGISTER_USER_BEGIN, SUCCESS, ERROR) as part of useReducer and AppContext components

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -2,13 +2,22 @@
 import React, { useReducer, useContext } from "react";
 
 import reducer from "./reducer";
-import { DISPLAY_ALERT, CLEAR_ALERT } from "./actions";
+import {
+  DISPLAY_ALERT,
+  CLEAR_ALERT,
+  REGISTER_USER_BEGIN,
+  REGISTER_USER_SUCCESS,
+  REGISTER_USER_ERROR,
+} from "./actions";
 
 export const initialState = {
   isLoading: false,
   showAlert: false,
   alertText: "",
   alertType: "",
+  user: null,
+  token: null,
+  userLocation: "",
 };
 
 const AppContext = React.createContext();
@@ -29,8 +38,13 @@ const AppProvider = ({ children }) => {
     }, 3000);
   };
 
+  //fetch function
+  const registerUser = async (currentUser) => {
+    console.log(currentUser);
+  };
+
   return (
-    <AppContext.Provider value={{ ...state, displayAlert }}>
+    <AppContext.Provider value={{ ...state, displayAlert, registerUser }}>
       {children}
     </AppContext.Provider>
   );
--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -1,4 +1,10 @@
-import { CLEAR_ALERT, DISPLAY_ALERT } from "./actions";
+import {
+  DISPLAY_ALERT,
+  CLEAR_ALERT,
+  REGISTER_USER_BEGIN,
+  REGISTER_USER_SUCCESS,
+  REGISTER_USER_ERROR,
+} from "./actions";
 
 const reducer = (state, action) => {
   //--- to be defined once actions.js is built
