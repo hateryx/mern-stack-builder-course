3005076d4faafeea2c67dc275b807f77b0ae3164
Richmond S. Damag <rsdamag.cpa.8@gmail.com>
2022-12-24 10:09:24 +0800

    Build: Initialize set-up for user login function
    - Defined the user login logic for usage from appContext.js thru Register page

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -21,7 +21,7 @@ const Register = () => {
   const navigate = useNavigate();
 
   // global context and useNavigate later
-  const { user, isLoading, showAlert, displayAlert, registerUser } =
+  const { user, isLoading, showAlert, displayAlert, registerUser, loginUser } =
     useAppContext();
 
   const changeHandler = (e) => {
@@ -37,7 +37,7 @@ const Register = () => {
     }
     const currentUser = { name, email, password };
     if (isMember) {
-      console.log("Already a member");
+      loginUser(currentUser);
     } else {
       registerUser(currentUser);
     }
