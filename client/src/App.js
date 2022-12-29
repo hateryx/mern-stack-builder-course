a090837e64bc19338f5d17fa214a55bba890ae99
Richmond S. Damag <rsdamag.cpa.8@gmail.com>
2022-12-30 03:27:04 +0800

    Set-up: Dashboard and Nested Pages
    - Initiated js files for the creation of dashboard and nested page structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Dashboard, Register, Landing, Error } from "./pages";
+import { Register, Landing, Error } from "./pages";
+export {
+  AddJob,
+  AllJobs,
+  Profile,
+  SharedLayout,
+  Stats,
+} from "./pages/dashboard";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Dashboard />} />
+          <Route path="/" element={<div>Dashboard</div>} />
           <Route path="/register" element={<Register />} />
           <Route path="/landing" element={<Landing />} />
           <Route path="*" element={<Error />} />
---
+++ client/src/pages/dashboard/SharedLayout.js
@@ -0,0 +1,5 @@
+const SharedLayout = () => {
+  return <h1>SharedLayout Page</h1>;
+};
+
+export default SharedLayout;
