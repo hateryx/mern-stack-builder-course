dd10279aa23cb5f35c7a8fc01f978ef216749d01
Richmond S. Damag <112183555+hateryx@users.noreply.github.com>
2023-01-01 19:40:37 +0800

    Update: Navbar -> Contents
    - Added buttons for toggle, dropdown and logout functions

--- client/src/component/Navbar.js
+++ client/src/component/Navbar.js
@@ -1,11 +1,36 @@
 import Wrapper from '../assets/wrappers/Navbar'
-import {FaHome} from 'react-icons/fa'
+import {FaAlignLeft, FaUserCircle, FaCaretDown, FaUser, FaCar} from 'react-icons/fa'
+import {useAppContext} from "../context/appContext"
+import Logo from './Logo'
 
 const Navbar = () => {
   return (
     <Wrapper>
-      <h4>Navbar</h4>
-      <FaHome/>
+      <div className='nav-center'>
+        <button type='button' className='toggle-btn' onClick={()=>console.log('toggle sidebar')}>
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h3 className='logo-text'>dashboard</h3>
+        </div>
+        <div className='btn-container'>
+          <button 
+            type='button' 
+            className='btn' 
+            onClick={()=>console.log('show/hide dropdown')}>
+              <FaUserCircle/>
+              John
+              <FaCaretDown/>
+            </button>
+            <div className='dropdown show-dropdown'>
+              <button type='button' className='dropdown-btn' onClick={()=>console.log('bye bye user')}>
+                logout
+             </button>
+            </div>
+        </div>
+      </div>
+      
       </Wrapper>
   )
 }
