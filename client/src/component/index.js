497a67c97c5701db3b5b116cfaa71549840e722e
Richmond S. Damag <112183555+hateryx@users.noreply.github.com>
2023-01-01 15:09:28 +0800

    Build: NavBar and Sidebar
    - Created the Navbar and Sidebars (Big and Small) components with Wrapper for css styling loaded
    - Defined the mentioned components into SharedLayout

--- client/src/component/index.js
+++ client/src/component/index.js
@@ -1,4 +1,9 @@
 import Logo from "./Logo";
 import FrontRow from "./FrontRow";
 import Alert from "./Alert";
-export { Logo, FrontRow, Alert };
+import Navbar from "./Navbar";
+import SmallSidebar from "./SmallSidebar";
+import BigSidebar from "./BigSidebar";
+
+
+export { Logo, FrontRow, Alert, Navbar, SmallSidebar, BigSidebar };
--- client/src/pages/dashboard/SharedLayout.js
+++ client/src/pages/dashboard/SharedLayout.js
@@ -1,15 +1,21 @@
 import { Outlet, Link } from "react-router-dom";
 import Wrapper from "../../assets/wrappers/SharedLayout";
-
+import {Navbar, BigSidebar,SmallSidebar} from '../../component'
 
 const SharedLayout = () => {
-  return <Wrapper>
-    <nav>
-      <Link to = "add-job">add job</Link>
-      <Link to = "all-jobs">all jobs</Link>
-    </nav>
-    <Outlet />
-  </Wrapper>;
+  return (
+  <Wrapper>
+    <main className="dashboard">
+      <SmallSidebar />
+      <BigSidebar />
+      <div>
+        <Navbar/>
+        <div className="dashboard-page">
+          <Outlet/>
+        </div>
+      </div>
+    </main>
+  </Wrapper>)
 };
 
 export default SharedLayout;
