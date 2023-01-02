fd6b0be0b16ce30c0a7be0cd64bdedd669b78462
Richmond S. Damag <112183555+hateryx@users.noreply.github.com>
2023-01-02 17:26:57 +0800

    Update: SmallSidebar Set-up
    Defined the contents needed for the showSidebar state toggles and arrangement of button and links (with placeholder)

--- client/src/component/SmallSidebar.js
+++ client/src/component/SmallSidebar.js
@@ -1,8 +1,27 @@
 import Wrapper from '../assets/wrappers/SmallSidebar'
+import links from '../utils/links'
+import Logo from './Logo'
+
+import { FaTimes } from 'react-icons/fa'
+import { NavLink } from 'react-router-dom'
+import { useAppContext } from '../context/appContext'
 
 const SmallSidebar = () => {
   return (
-    <Wrapper><h4>Small Sidebar</h4></Wrapper>
+    <Wrapper>
+      <div className='sidebar-container show-sidebar'>
+        <div className="content">
+          <button type= "button" className='close-btn' onClick={()=> console.log("toggle")}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+      
+        <div className='nav-links'>nav links</div>
+        </div>
+      </div>
+    </Wrapper>
   )
 }
 
