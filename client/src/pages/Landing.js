3e996d1ed5f79765a2132307042eb7e25065e6af
Richmond S. Damag <112183555+hateryx@users.noreply.github.com>
2023-01-08 14:20:06 +0800

    Update: Landing Page
    - Updated the write-up showcase for the landing page

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -17,14 +17,14 @@ const Landing = () => {
           <h1>
             job <span>tracking</span> app
           </h1>
-          <p>
-            I'm baby tbh pork belly poutine, af kombucha +1 vegan. Scenester
-            quinoa sustainable umami fit vinyl +1 cloud bread farm-to-table
-            mukbang irony tilde. Cornhole vinyl hella, taxidermy humblebrag
-            glossier austin. Gluten-free palo santo forage knausgaard, fixie
-            small batch kitsch jianbing marfa selvage air plant lumbersexual
-            yuccie mustache.
-          </p>
+        
+ 
+  <p>The ultimate tool for helping you find your dream job! 
+    Our app offers a range of features to help you stay organized and on top of your job search.</p>
+  <p>Keep track of all your job applications in one place. Our app allows you to easily see the status of each application and set reminders for follow-up tasks.</p>
+  <p>Don't waste any more time on a disorganized job search. Try our job tracking app today and take control of your career!</p>
+
+
           <Link to="/register" className="btn btn-hero">
             Login/Register
           </Link>
