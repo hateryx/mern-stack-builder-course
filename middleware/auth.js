350879b65ccc450143ce7594452c767cda1e5594
Richmond S. Damag <112183555+hateryx@users.noreply.github.com>
2023-01-08 14:27:47 +0800

    Update: middleware/auth.js
    - Defined verification procedures using jsonwebtoken npm as setup for the update user function

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,16 +1,28 @@
 import { UnAuthenticatedError } from '../errors/index.js'
+import jwt from 'jsonwebtoken'
 
-
+UnAuthenticatedError
 const auth = async (req, res, next) => {
     //const header = req.headers
     //console.log(header)
 
     const authHeader = req.headers.authorization
-    if (!authHeader) {
+    if (!authHeader || !authHeader.startsWith('Bearer')) {
         throw new UnAuthenticatedError('Authentication Invalid')
     }
-    console.log(authHeader)
-    next()
+    //console.log(authHeader)
+    const token = authHeader.split(' ')[1]
+
+    try {
+        const payload = jwt.verify(token, process.env.JWT_SECRET)
+        //console.log(payload)
+        //attach the user request object
+        //req.user = payload
+        req.user = { userId: payload.userId }
+        next()
+    } catch (error){
+        throw new UnAuthenticatedError('Authentication Invalid')
+    }
 }
 
 export default auth
