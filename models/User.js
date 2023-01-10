6efd76527b117f64018b4153992a784aed6a7156
Richmond S. Damag <112183555+hateryx@users.noreply.github.com>
2023-01-10 21:12:36 +0800

    Update: User Schema
    - Defined function using modifiedPath to state that if the password is not updated in a register session, the existing password shall be hashed using bcrypt npm

--- models/User.js
+++ models/User.js
@@ -45,8 +45,10 @@ const UserSchema = new mongoose.Schema({
 //middleware set-up
 //this function defines instruction to do certain functions before saving record into the database
 UserSchema.pre("save", async function () {
-  //const salt = await bcrypt.genSalt(10);
-  //this.password = await bcrypt.hash(this.password, salt);
+  console.log(this.modifiedPaths())
+  if(!this.isModified('password')) return 
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 UserSchema.methods.createJWT = function () {
