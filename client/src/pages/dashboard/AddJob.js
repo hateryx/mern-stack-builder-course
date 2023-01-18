10b05b3d8ab8bf534cc65ae9c164141271d5513a
Richmond S. Damag <112183555+hateryx@users.noreply.github.com>
2023-01-18 21:36:22 +0800

    Update: Add Job Page
    - Built the structure for add job page

--- client/src/pages/dashboard/AddJob.js
+++ client/src/pages/dashboard/AddJob.js
@@ -1,5 +1,80 @@
+import { FrontRow, Alert } from "../../component";
+import { useAppContext } from "../../context/appContext";
+import Wrapper from "../../assets/wrappers/DashboardFormPage";
+
 const AddJob = () => {
-  return <h1>Add Job page</h1>;
+  const {
+    isEditing,
+    showAlert,
+    displayAlert,
+    position,
+    company,
+    jobLocation,
+    jobType,
+    jobTypeOptions,
+    status,
+    statusOptions,
+  } = useAppContext()
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if(!position || !company || !jobLocation) {
+      displayAlert()
+      return
+    }
+    console.log("Create Job")
+  }
+  
+  const handleJobInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    console.log(`${name}: ${value}`)
+  }
+
+  
+
+  return <Wrapper>
+    <form className="form">
+      <h3>{isEditing ? "edit job" : "add job"}</h3>
+      {showAlert && <Alert />}
+      <div className="form-center">
+        {/* position */}
+        <FrontRow 
+          type="text" 
+          name="position" 
+          value={position} 
+          changeHandler={handleJobInput}
+        />
+
+         {/* company */}
+         <FrontRow 
+          type="text" 
+          name="company" 
+          value={company} 
+          changeHandler={handleJobInput}
+        />
+
+         {/* location */}
+         <FrontRow 
+          type="text" 
+          labelText = "Job Location"
+          name="jobLocation" 
+          value={jobLocation} 
+          changeHandler={handleJobInput}
+        />
+        {/* job type */}
+        {/* job status */}
+        <div className="btn-container">
+          <button type="submit" className="btn btn-block submit-btn" onClick={handleSubmit}>
+            submit
+          </button>
+        </div>
+
+      </div>
+    </form>
+  </Wrapper>
 };
 
 export default AddJob;
