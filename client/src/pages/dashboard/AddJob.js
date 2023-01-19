f1146e361578ddbdce09a3d2cff3ee071f142cd2
Richmond S. Damag <112183555+hateryx@users.noreply.github.com>
2023-01-19 21:10:02 +0800

    Update: AddJob -> Select Job
    - Added Select Job Type and options for the value using map function from the job type options

--- client/src/pages/dashboard/AddJob.js
+++ client/src/pages/dashboard/AddJob.js
@@ -14,67 +14,88 @@ const AddJob = () => {
     jobTypeOptions,
     status,
     statusOptions,
-  } = useAppContext()
-
+  } = useAppContext();
 
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    if(!position || !company || !jobLocation) {
-      displayAlert()
-      return
+    if (!position || !company || !jobLocation) {
+      displayAlert();
+      return;
     }
-    console.log("Create Job")
-  }
-  
+    console.log("Create Job");
+  };
+
   const handleJobInput = (e) => {
     const name = e.target.name;
     const value = e.target.value;
-    console.log(`${name}: ${value}`)
-  }
+    console.log(`${name}: ${value}`);
+  };
 
-  
+  return (
+    <Wrapper>
+      <form className="form">
+        <h3>{isEditing ? "edit job" : "add job"}</h3>
+        {showAlert && <Alert />}
+        <div className="form-center">
+          {/* position */}
+          <FrontRow
+            type="text"
+            name="position"
+            value={position}
+            changeHandler={handleJobInput}
+          />
 
-  return <Wrapper>
-    <form className="form">
-      <h3>{isEditing ? "edit job" : "add job"}</h3>
-      {showAlert && <Alert />}
-      <div className="form-center">
-        {/* position */}
-        <FrontRow 
-          type="text" 
-          name="position" 
-          value={position} 
-          changeHandler={handleJobInput}
-        />
+          {/* company */}
+          <FrontRow
+            type="text"
+            name="company"
+            value={company}
+            changeHandler={handleJobInput}
+          />
 
-         {/* company */}
-         <FrontRow 
-          type="text" 
-          name="company" 
-          value={company} 
-          changeHandler={handleJobInput}
-        />
-
-         {/* location */}
-         <FrontRow 
-          type="text" 
-          labelText = "Job Location"
-          name="jobLocation" 
-          value={jobLocation} 
-          changeHandler={handleJobInput}
-        />
-        {/* job type */}
-        {/* job status */}
-        <div className="btn-container">
-          <button type="submit" className="btn btn-block submit-btn" onClick={handleSubmit}>
-            submit
-          </button>
+          {/* location */}
+          <FrontRow
+            type="text"
+            labelText="Job Location"
+            name="jobLocation"
+            value={jobLocation}
+            changeHandler={handleJobInput}
+          />
+          {/* job type */}
+          <div className="form-row">
+            <label htmlFor="jobType" className="form-label">
+              job type
+            </label>
+            <select
+              name="jobType"
+              value={jobType}
+              changeHandler={handleJobInput}
+              className="form-select"
+            >
+              {jobTypeOptions.map((itemValue, index) => {
+                return (
+                  <option key={index} value={itemValue}>
+                    {itemValue}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          {/* job status */}
+          <div className="btn-container">
+            <button
+              type="submit"
+              className="btn btn-block submit-btn"
+              onClick={handleSubmit}
+            >
+              submit
+            </button>
+          </div>
         </div>
-
-      </div>
-    </form>
-  </Wrapper>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default AddJob;
