71a0408ef526d1028f32c11d55249bc1e6e3b1d6
Richmond S. Damag <112183555+hateryx@users.noreply.github.com>
2023-01-19 22:05:08 +0800

    Update: index component
    - Added the FrontRowSelect as part of component

--- client/src/component/index.js
+++ client/src/component/index.js
@@ -4,6 +4,14 @@ import Alert from "./Alert";
 import Navbar from "./Navbar";
 import SmallSidebar from "./SmallSidebar";
 import BigSidebar from "./BigSidebar";
+import FrontRowSelect from "./FrontRowSelect";
 
-
-export { Logo, FrontRow, Alert, Navbar, SmallSidebar, BigSidebar };
+export {
+  Logo,
+  FrontRow,
+  Alert,
+  Navbar,
+  SmallSidebar,
+  BigSidebar,
+  FrontRowSelect,
+};
