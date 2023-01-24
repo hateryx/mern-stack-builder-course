728f4761d0b5bc0e437d5df33771f94521971ee0
Richmond S. Damag <112183555+hateryx@users.noreply.github.com>
2023-01-24 23:26:09 +0800

    Update Landing.js

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -17,13 +17,18 @@ const Landing = () => {
           <h1>
             job <span>tracking</span> app
           </h1>
-        
- 
-  <p>The ultimate tool for helping you find your dream job! 
-    Our app offers a range of features to help you stay organized and on top of your job search.</p>
-  <p>Keep track of all your job applications in one place. Our app allows you to easily see the status of each application and set reminders for follow-up tasks.</p>
-  <p>Don't waste any more time on a disorganized job search. Try our job tracking app today and take control of your career!</p>
 
+          <p>
+            The ultimate tool for helping you find your dream job! Our app
+            offers a range of features to help you stay organized and on top of
+            your job search.
+          </p>
+          <p>
+            Keep track of all your job applications in one place. Our app allows
+            you to easily see the status of each application and set reminders
+            for follow-up tasks.
+          </p>
+          <p>Try our job tracking app today and take control of your career!</p>
 
           <Link to="/register" className="btn btn-hero">
             Login/Register
